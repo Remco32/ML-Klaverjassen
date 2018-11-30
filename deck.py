08d8861f8470bcbcbd4dbd4f7664a4ee23eadc64
Remco32 <remco32@gmail.com>
2018-11-30 14:40:28 +0100

    Renamed file to deck.py. Added creating hands for players, and a function to receive them.

--- deck.py
+++ deck.py
@@ -10,6 +10,14 @@ deckDict = {
     's': {'K': 4, 'Q': 3, 'J': 2, '10': 10, '9': 0, '8': 0, '7': 0, 'A': 11}
     }
 
+# To save the divided cards for each players after shuffling, but before handing the cards over to the player
+handPlayer1 = []
+handPlayer2 = []
+handPlayer3 = []
+handPlayer4 = []
+
+dividedCards = [handPlayer1, handPlayer2, handPlayer3, handPlayer4]
+
 
 # class to represent a card in the game
 ##the value attribute is kept separate cause it is conceptually different:
@@ -38,13 +46,32 @@ maxValue = deckDict['d']['K']  # the highest non-trump value
 # more valuable than any other non-trump card
 [card.SetValue(deckDict[card.suit][card.name]) for card in deck]
 [card.SetValue(deckDict[card.suit][card.name] + maxValue) for card in deck if card.suit == trump]
-print("Deck:")
-[print(card.name, card.suit, card.value) for card in deck]
 
 # now to the deck shuffling
 import random as rnd
 
 rnd.shuffle(deck)
-print("Shuffled deck:")
-print('\n')
-[print(card.name, card.suit, card.value) for card in deck]
+shuffledDeck = [(card.name, card.suit, card.value) for card in deck]
+
+# Split the shuffled deck in four parts, so each player can receive their hand
+# TODO: Change prints to something actually functional
+def divideCards():
+    # Each player will receive 32/4=8 cards
+
+    for cardIndex in range(len(shuffledDeck)):
+        if cardIndex >= 0 and cardIndex <= 7:
+            handPlayer1.append(shuffledDeck[cardIndex])
+        if cardIndex >= 8 and cardIndex <= 15:
+            handPlayer2.append(shuffledDeck[cardIndex])
+        if cardIndex >= 16 and cardIndex <= 23:
+            handPlayer3.append(shuffledDeck[cardIndex])
+        if cardIndex >= 24 and cardIndex <= 32:
+            handPlayer4.append(shuffledDeck[cardIndex])
+
+# A player can request his hand from the deck using this function
+# Given argument starts counting from 0.
+def handOutCards(playerNumber):
+    return dividedCards[playerNumber]
+
+divideCards()
+print("Done")
