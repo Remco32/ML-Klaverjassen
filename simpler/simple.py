be1418e151d9c54d8d0580c23848c7e24b5a9202
amlaeb <tisol@tuta.io>
2018-12-18 12:12:53 +0100

    Rewritten and updated simple.py

--- simpler/simple.py
+++ simpler/simple.py
@@ -1,8 +1,3 @@
-"""
-Simpler version of a card game to try out some reinforcement learning
-algorithms and mechanics
-"""
-
 import time
 import numpy as np
 import matplotlib
@@ -12,167 +7,167 @@ import torch
 import torch.nn as nn
 import pdb
 
-#learning rate and discount rate and epochs
-alpha, y, epochs, savingEpoch, printEpoch = 0.01, 0.9, 30000, 30, 5
+
+
+
+#VARIABLES
+alpha, y, epoch, savingEpoch, printEpoch = 0.01, 0.9, 3000, 300, 30
+rew1, rew2 = [], []
+r1Win, r1Lose, r2Win, r2Lose = 3, -0.5, 2, -0.75
+
+#load parameters?
+loadP = 1
 #to save the weights
 FOLDER = '/Users/tommi/github/ML-Klaverjassen/simpler/weights/'
 PATH1 = FOLDER + 'net1_weights.pth'
 PATH2 = FOLDER + 'net2_weights.pth'
 
-#the network
+
+
+
+#THE NETWORK
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conn1 = nn.Linear(12,7)  
-        self.conn4 = nn.Linear(7,6)
+        self.conn1 = nn.Linear(12,20)
+        self.conn2 = nn.Linear(20,6)
     def forward(self, x):
         x = torch.sigmoid(self.conn1(x))
-        x = self.conn4(x)
+        x = self.conn2(x)
         return x
 
-    
-#net for the two players
-net1 = Net()
-net2 = Net()
-    
+#nets for the players   
+n1 = Net()
+n2 = Net()
+
 #load parameters from previous training rounds
+if loadP == 1:
+    n1.load_state_dict(torch.load(PATH1))
+    n2.load_state_dict(torch.load(PATH2))
+    print('Loaded model parameters from folder {}'.format(FOLDER))
 
-net1.load_state_dict(torch.load(PATH1))
-net2.load_state_dict(torch.load(PATH2))
-print('Loaded model parameters from folder {}'.format(FOLDER))
-
-
-
-#stochastic gradient descent optimiser
-opt1 = torch.optim.SGD(net1.parameters(), lr=alpha, momentum=0.9)
-opt2 = torch.optim.SGD(net2.parameters(), lr=alpha, momentum=0.9)
+#optimisers and losses
+opt1  = torch.optim.SGD(n1.parameters(), lr = alpha)
+opt2  = torch.optim.SGD(n2.parameters(), lr = alpha)
 loss1 = nn.MSELoss()
 loss2 = nn.MSELoss()
 
 
 
-#start counter
+
+
+
+#THE GAME
 start = time.time()
+for i in range(epoch):
 
-#to keep track of the total reward obtained by player 2 (who in this simple
-#game should always win)
-rew2 = []
-
-#start the training cycle
-for i in range(epochs):
-
-    #re-initialise the feature vectors
+    id1, id2 = [0, 2, 4], [1, 3, 5]
     p1 = torch.zeros(12, dtype=torch.float, requires_grad=True)
     p2 = torch.zeros(12, dtype=torch.float, requires_grad=True)
-    p1_id, p2_id = [0,2,4], [1,3,5]
     with torch.no_grad():
         for k in range(12):
-            if k in p1_id:
+            if k in id1:
                 p1[k] = 1
-            elif k in p2_id:
+            elif k in id2:
                 p2[k] = 1
 
-        
-    #print('\nRound {} \tNew hands:\n{},\n{}'.format(i,p1,p2))
     for j in range(3):
         opt1.zero_grad()
         opt2.zero_grad()
-        out1 = net1(p1)
-        out2 = net2(p2)
-        a1 = out1.argmax()
-        a2 = out2.argmax()
-        
+        out1 = n1(p1)
+        out2 = n2(p2)
+        a1   = out1.argmax().item()
+        a2   = out2.argmax().item()
 
-        while a1.item() not in p1_id:     #learn that it's a mistake
-            #pdb.set_trace()
+        while a1 not in id1:
             r1 = -1
+            rew1.append(r1)
             with torch.no_grad():
-                P1 = p1.clone()
-                _Q1 = net1(P1)
                 Q1 = out1.clone()
-            Q1[a1] += alpha * ( r1 + y * torch.max(_Q1).item() - Q1[a1])
-            l1 = loss1(Q1, out1)              #square loss
+                Q1[a1] += alpha * ( r1 + y * torch.max(out1).item() - Q1[a1])
+            l1 = loss1(Q1, out1)
             l1.backward()
             opt1.step()
             opt1.zero_grad()
-            out1 = net1(p1)
-            a1 = out1.argmax()
+            out1 = n1(p1)
+            a1   = out1.argmax().item()
 
-        while a2.item() not in p2_id:     #learn that it's a mistake
-            #pdb.set_trace()
+        while a2 not in id2:
             r2 = -1
             rew2.append(r2)
             with torch.no_grad():
-                P2 = p2.clone()
-                _Q2 = net2(P2)
                 Q2 = out2.clone()
-            Q2[a2] += alpha * ( r2 + y * torch.max(_Q2).item() - Q2[a2])
-            l2 = loss2(Q2, out2)              #square loss
+                Q2[a2] += alpha * ( r2 + y * torch.max(out2).item() - Q2[a2])
+            l2 = loss2(Q2, out2)
             l2.backward()
             opt2.step()
             opt2.zero_grad()
-            out2 = net2(p2)
-            a2 = out2.argmax()
-        
-        #Simple game rule: the highest index wins
-        if a1.item() > a2.item():
-            r1 = a1.item() + a2.item()
-            r2 = 0           #not as bad as playing the wrong card
+            out2 = n2(p2)
+            a2   = out2.argmax().item()            
+
+        if a1 > a2:
+            r1 = r1Win
+            r2 = r2Lose
+            rew1.append(r1)
             rew2.append(r2)
-        elif a2.item() > a1.item():
-            r2 = a1.item() + a2.item()
-            r1 = 0
+        if a2 > a1:
+            r2 = r2Win
+            r1 = r1Lose
+            rew1.append(r1)
             rew2.append(r2)
-        
+
         with torch.no_grad():
             P1 = p1.clone()
             P2 = p2.clone()
             P1[a1] = 0
             P2[a2] = 0
             P1[6 + a1] = 1
-            P1[6 + a2] = 1
+            P1[6 + a2] = 2       #major change
             P2[6 + a1] = 1
-            P2[6 + a2] = 1
-        
-        p1_id.remove(a1.item())
-        p2_id.remove(a2.item())
-        opt1.zero_grad()
-        opt2.zero_grad()
+            P2[6 + a2] = 2       #major change
+
+        id1.remove(a1)
+        id2.remove(a2)
         with torch.no_grad():
-            _Q1 = net1(P1)          #next state Q values
-            _Q2 = net2(P2)
-            Q1  = out1.clone() 
-            Q2  = out2.clone()
-        Q1[a1] += alpha * ( r1 + y * torch.max(_Q1).item() - Q1[a1])
-        Q2[a2] += alpha * ( r2 + y * torch.max(_Q2).item() - Q2[a2])
-        #pdb.set_trace()
+            Q1 = out1.clone()
+            Q2 = out2.clone()
+        q1 = n1(P1)
+        q2 = n2(P2)
+        Q1[a1] += alpha * ( y * torch.max(q1).item() - Q1[a1])
+        Q2[a2] += alpha * ( y * torch.max(q2).item() - Q2[a2])
         l1 = loss1(Q1, out1)
-        l2 = loss2(Q2, out2)
+        l2 = loss2(Q1, out2)
         l1.backward()
         l2.backward()
         opt1.step()
         opt2.step()
-        p1 = P1.clone()
-        p2 = P2.clone()
-        #print('Hands after playing:\n{},\n{}'.format(p1,p2))
+        p1, p2 = P1.clone(), P2.clone()
+        p1.requires_grad, p2.requires_grad = True, True
+        
     if i % printEpoch == 0:
-        print('Epoch {} of {}'.format(i,epochs))
+        print('Epoch {} of {}'.format(i,epoch))
     if i  % savingEpoch == 0:
-        torch.save(net1.state_dict(), PATH1)
-        torch.save(net2.state_dict(), PATH2)
-        print('Epoch {} of {}\nSaved weight files in {}'.format(i,epochs,FOLDER))
+        torch.save(n1.state_dict(), PATH1)
+        torch.save(n2.state_dict(), PATH2)
+        print('Epoch {} of {}\nSaved weight files in {}'.format(i,epoch,FOLDER))
 
 
-torch.save(net1.state_dict(), PATH1)
-torch.save(net2.state_dict(), PATH2)
-print('\nSaved weight files in folder {}'.format(FOLDER))
+        
+
+        
+#AFTER THE GAME
+#save params
+torch.save(n1.state_dict(), PATH1)
+torch.save(n2.state_dict(), PATH2)
+print('Epoch {} of {}\nSaved weight files in {}'.format(i,epoch,FOLDER))
 
 #calculate total training time
 elapsed = time.time() - start
 print('\n\nTotal training time: {:.6}'.format(elapsed))
 
-#plot the reward for player 2
-r = np.array(rew2)
-plt.plot(r,'^g')
+#plot the reward
+r1 = np.array(rew1)
+r2 = np.array(rew2)
+plt.plot(r1,'ob')
+plt.plot(r2,'xr')
 plt.show()
-
