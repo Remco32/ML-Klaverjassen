17f62942c60701aefe1dc52fbe4502a675d80b49
amlaeb <tisol@tuta.io>
2018-12-19 17:36:21 +0100

    Simple.py working perfectly
    Finally simple.py works and learns exactly as expected. After extensive training (around 100k epochs, took about 5 minutes for me), player 2 (whose cards can beat all of player 1's cards) always wins each trick, and player 1 always loses. This version also includes keeping track of the card which is currently on the table.
    
    From now this version is considered definitive and working, so now it's the time to apply these methods to Klaverjassen!

--- simpler/simple.py
+++ simpler/simple.py
@@ -10,13 +10,14 @@ import pdb
 
 
 
+
 #VARIABLES
-alpha, y, epoch, savingEpoch, printEpoch = 0.01, 0.9, 300, 300, 30
+alpha, y, epoch, savingEpoch, printEpoch = 0.01, 0.9, 300000, 30000, 6000
 rew1, rew2 = [], []
-r1Win, r1Lose, r2Win, r2Lose = 3, -0.25, 2, -0.5
+r1Win, r1Lose, r2Win, r2Lose = 2, -0.3, 2, -0.3
 
 #load parameters?
-loadP = 1
+loadP = 0
 #to save the weights
 FOLDER = '/Users/tommi/github/ML-Klaverjassen/simpler/weights/'
 PATH1 = FOLDER + 'net1_weights.pth'
@@ -25,11 +26,13 @@ PATH2 = FOLDER + 'net2_weights.pth'
 
 
 
+
+
 #THE NETWORK
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conn1 = nn.Linear(12,20)
+        self.conn1 = nn.Linear(13,20)
         self.conn2 = nn.Linear(20,6)
     def forward(self, x):
         x = torch.sigmoid(self.conn1(x))
@@ -44,7 +47,7 @@ n2 = Net()
 if loadP == 1:
     n1.load_state_dict(torch.load(PATH1))
     n2.load_state_dict(torch.load(PATH2))
-    print('Loaded model parameters from folder {}'.format(FOLDER))
+    print('Loaded model parameters from {}'.format(FOLDER))
 
 #optimisers and losses
 opt1  = torch.optim.SGD(n1.parameters(), lr = alpha)
@@ -62,8 +65,8 @@ start = time.time()
 for i in range(epoch):
 
     id1, id2 = [0, 2, 4], [1, 3, 5]
-    p1 = torch.zeros(12, dtype=torch.float, requires_grad=True)
-    p2 = torch.zeros(12, dtype=torch.float, requires_grad=True)
+    p1 = torch.zeros(13, dtype=torch.float, requires_grad=True)
+    p2 = torch.zeros(13, dtype=torch.float, requires_grad=True)
     with torch.no_grad():
         for k in range(12):
             if k in id1:
@@ -89,14 +92,12 @@ for i in range(epoch):
             out1 = n1(p1)
             a1   = out1.argmax().item()
 
+       # print('\n',j,a1)
+       # tmp = input('--continue?')
         with torch.no_grad():
-            P1 = p1.clone()
             P2 = p2.clone()
-            P1[6 + a1] = 1
-            P2[6 + a1] = 1
-            p1 = P1.clone()
-            p2 = P1.clone()
-            p1.requires_grad = True
+            P2[6] = a1     #to keep track of the played card
+            p2 = P2.clone()
             p2.requires_grad = True
             
         opt2.zero_grad()
@@ -115,26 +116,33 @@ for i in range(epoch):
             opt2.zero_grad()
             out2 = n2(p2)
             a2   = out2.argmax().item()            
-
+       # print(a2)
+       # tmp = input('--continue?')
         if a1 > a2:
             r1 = r1Win
             r2 = r2Lose
             rew1.append(r1)
             rew2.append(r2)
+        #    print('Winner: p1')
+         #   tmp = input('--continue?')
         if a2 > a1:
             r2 = r2Win
             r1 = r1Lose
             rew1.append(r1)
             rew2.append(r2)
+          #  print('Winner: p2')
+           # tmp = input('--continue?')
 
         with torch.no_grad():
             P1 = p1.clone()
             P2 = p2.clone()
             P1[a1] = 0
             P2[a2] = 0
-            P1[6 + a2] = 2       #major change
-            P2[6 + a2] = 2       #major change
-
+            P1[7 + a2] = 2       #major change
+            P1[7 + a1] = 1
+            P2[7 + a2] = 2       #major change
+            P2[7 + a1] = 1
+            
         id1.remove(a1)
         id2.remove(a2)
         with torch.no_grad():
@@ -145,7 +153,7 @@ for i in range(epoch):
         Q1[a1] += alpha * ( y * torch.max(q1).item() - Q1[a1])
         Q2[a2] += alpha * ( y * torch.max(q2).item() - Q2[a2])
         l1 = loss1(Q1, out1)
-        l2 = loss2(Q1, out2)
+        l2 = loss2(Q2, out2)
         l1.backward()
         l2.backward()
         opt1.step()
@@ -154,11 +162,11 @@ for i in range(epoch):
         p1.requires_grad, p2.requires_grad = True, True
         
     if i % printEpoch == 0:
-        print('Epoch {} of {}'.format(i,epoch))
+        print('Epoch {} of {}\t\tElapsed time: {:.6} s'.format(i,epoch, time.time()-start))
     if i  % savingEpoch == 0:
         torch.save(n1.state_dict(), PATH1)
         torch.save(n2.state_dict(), PATH2)
-        print('Epoch {} of {}\nSaved weight files in {}'.format(i,epoch,FOLDER))
+        print('Saved weight files in {}'.format(FOLDER))
 
 
         
@@ -168,15 +176,15 @@ for i in range(epoch):
 #save params
 torch.save(n1.state_dict(), PATH1)
 torch.save(n2.state_dict(), PATH2)
-print('Epoch {} of {}\nSaved weight files in {}'.format(i,epoch,FOLDER))
+print('Finished learning! Saved weight files in {}'.format(FOLDER))
 
 #calculate total training time
 elapsed = time.time() - start
-print('\n\nTotal training time: {:.6}'.format(elapsed))
+print('\n\nTotal training time: {:.6} s'.format(elapsed))
 
 #plot the reward
 r1 = np.array(rew1)
 r2 = np.array(rew2)
 plt.plot(r1,'-b')
-plt.plot(r2,'.r')
+plt.plot(r2,'-r')
 plt.show()
