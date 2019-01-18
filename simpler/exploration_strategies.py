6b7ce9d67177165cd06bfade3a99501f01330fdc
Remco32 <remco32@gmail.com>
2019-01-18 16:50:57 +0100

    Small adjustment to expl. strats to bring it up to date with the other branch.

--- simpler/exploration_strategies.py
+++ simpler/exploration_strategies.py
@@ -2,26 +2,28 @@ import random as rnd
 
 #TODO write a main class for handling all sorts of exploration strategies, i.e. you only have to call the main function of this file, not a sub function
 
-def diminishingEpsilonGreedy(greedyActionIndex, epsilon, sizeOfPlayerHand, currentEpoch, totalEpocs):
+def diminishingEpsilonGreedy(greedyAction, epsilon, playerHand, currentEpoch, totalEpocs):
 # Diminishing epsilon-Greedy
-
     # Set current exploration rate
     minimumExplorationRate = 0.01
-
     explorationRate = epsilon * (1 - (currentEpoch / totalEpocs))
 
     # Check for minimum limit exploration rate
-
     if explorationRate < minimumExplorationRate:
         explorationRate = minimumExplorationRate
 
     # Rest of the algorithm is essentially epsilon-greedy
-    return epsilonGreedy(greedyActionIndex, explorationRate, sizeOfPlayerHand)
+    return epsilonGreedy(greedyAction, explorationRate, playerHand)
 
 
-def epsilonGreedy(greedyActionIndex, explorationRate, sizeOfPlayerHand):
-    random = rnd.uniform(0, 1)
-    if random < explorationRate:
-        return rnd.choice(sizeOfPlayerHand) #size instead of actual hand so so we can return an index
+def epsilonGreedy(greedyAction, explorationRate, playerHand):
+    randomValue = rnd.uniform(0, 1) #between 0 and 1, float
+    if randomValue < explorationRate:
+        return rnd.choice(playerHand)
     else:
-        return greedyActionIndex
+        return greedy(greedyAction)
+    
+#TODO: Placeholder. We can implement the function that picks the best legal move here
+def greedy(greedyAction):
+    return greedyAction
+
